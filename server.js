2cc6befeb16643873dbc49ea086a2a0b031b1684
ADISAK02 <smaris.ws02@gmail.com>
2023-09-06 15:47:51 +0700

    update remove request

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const Product = require('./models/product')
 
 const cors = require('cors');
-const req = require('express/lib/request');
 app.use(cors());
 mongoose.connect(
     'mongodb+srv://root:631411015@cluster0.8tpnnbx.mongodb.net/?retryWrites=true&w=majority',
@@ -19,7 +18,7 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.json("Hello World");
-    
+
 })
 
 app.get('/getallproduct', async (req, res) => {
